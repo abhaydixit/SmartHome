213860df850763b1a3502ea9c1fce0f09abae65c
abhishek <abhikori1994@gmail.com>
2017-01-14 15:54:42 +0530

    made it auto fetch

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Test {
+public class Test extends TimerTask{
 	
 
 	
@@ -14,21 +14,30 @@ public class Test {
 		
 		//System.out.println(Test.class.getClass().getCanonicalName());
 		///home/abhishek/softwares/Jess71p2/bin/jess
+		Timer timer = new Timer();
+		timer.schedule(new Test(),0,1000);
+		
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 		
 		Rete engine = new Rete();
 		try {
 			engine.executeCommand("(clear)");
-			engine.executeCommand("(watch all)");
+			//engine.executeCommand("(watch all)");
 
 			engine.executeCommand("(deftemplate house (slot rain) (slot sprinkler) (slot window))");
 
-			engine.executeCommand("(defrule is-it-raining ?res <- (house (rain ?r&:(> ?r 0.5))) =>  (printout t \"close the window\"))");
-			engine.executeCommand("(defrule its-not-raining ?res <- (house (rain ?r&:(< ?r 0.5))) =>  (printout t \"keep the window open\"))");
+			engine.executeCommand("(defrule is-it-raining ?res <- (house (rain ?r&:(> ?r 0.5))) =>  (printout t \" \n close the window\"))");
+			engine.executeCommand("(defrule its-not-raining ?res <- (house (rain ?r&:(< ?r 0.5))) =>  (printout t \" \n keep the window open\"))");
 			//float rain;
 			String rain ="0";
 	        try {
 				  rain = new String(Files.readAllBytes(Paths.get("./rain.txt")));
-				  System.out.println("The rain value is "+rain);
+				 // System.out.println("The rain value is "+rain);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
