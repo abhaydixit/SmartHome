dd17ba2c05ed89db433b9a59427275a311f91c5f
abhishek <abhikori1994@gmail.com>
2017-03-20 14:24:42 +0530

    first commit

---
+++ src/Check.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Check extends TimerTask{
+	int version;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		String rain;
+		
+		try {
+			rain = new String(Files.readAllBytes(Paths.get("/home/abhishek/java_workspace/JESS/check.txt")));
+			
+			  System.out.println("The rain value is "+rain);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	public static void main(String args[]) {
+		Timer timer = new Timer();
+		timer.schedule(new Check(),0,1000);
+		
+		
+	}
+
+}
---
+++ src/Test.java
@@ -0,0 +1,49 @@
+import jess.*;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Test {
+	
+
+	
+	
+	
+	public static void main(String args[]) {
+		
+		//System.out.println(Test.class.getClass().getCanonicalName());
+		
+		Rete engine = new Rete();
+		try {
+			engine.executeCommand("(clear)");
+			engine.executeCommand("(watch all)");
+
+			engine.executeCommand("(deftemplate house (slot rain) (slot sprinkler) (slot window))");
+
+			engine.executeCommand("(defrule is-it-raining ?res <- (house (rain ?r&:(> ?r 0.5))) =>  (printout t \"close the window\"))");
+			engine.executeCommand("(defrule its-not-raining ?res <- (house (rain ?r&:(< ?r 0.5))) =>  (printout t \"keep the window open\"))");
+			//float rain;
+			String rain ="0";
+	        try {
+				  rain = new String(Files.readAllBytes(Paths.get("/home/abhishek/java_workspace/JESS/rain.txt")));
+				  System.out.println("The rain value is "+rain);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+	        
+
+			String cmd="(assert (house (rain "+rain+")))";
+			engine.executeCommand(cmd);
+			engine.executeCommand("(run)");
+
+			
+		} catch (JessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
